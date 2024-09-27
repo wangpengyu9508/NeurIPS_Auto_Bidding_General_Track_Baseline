9399494f83a862a0d936fce979344027570d4626
wangpengyu03 <wangpengyu03@wangpengyudeMacBook-Pro.local>
2024-09-27 15:30:48 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker build -t registry.cn-beijing.aliyuncs.com/nips_train_env_ypj/train_env:balalanlxmx -f ./Dockerfile .
-docker push registry.cn-beijing.aliyuncs.com/nips_train_env_ypj/train_env:balalanlxmx
+docker build -t registry.cn-beijing.aliyuncs.com/nips_train_env_ypj/train_env:bc_v1 -f ./Dockerfile .
+docker push registry.cn-beijing.aliyuncs.com/nips_train_env_ypj/train_env:bc_v1
