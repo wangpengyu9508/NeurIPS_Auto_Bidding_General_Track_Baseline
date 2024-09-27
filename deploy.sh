ba2784105d8315971eadba050863ccd5af63f15e
wangpengyu03 <wangpengyu03@wangpengyudeMacBook-Pro.local>
2024-09-27 13:03:44 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 docker build -t crpi-ggbwo0f3aq6i02ek.cn-beijing.personal.cr.aliyuncs.com/nips_train_wpy/train_env:balalanlxmx -f ./Dockerfile .
 docker push crpi-ggbwo0f3aq6i02ek.cn-beijing.personal.cr.aliyuncs.com/nips_train_wpy/train_env:balalanlxmx
-
